c6a4a18fa71f311c0aeec320e2f7c0e0e2132aa0
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-01 17:21:30 -0300

    Using component properties

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <h1>Olá Mundo!!</h1>
+      <h1>{props.value}</h1>
     </div>
   );
 }
