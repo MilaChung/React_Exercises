b825e11f72aba491d6ad2cb2178715e6f9a76488
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-01 20:09:45 -0300

    Class Component

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Family from './components/family';
-import Member from './components/member';
+import ClassComponent from './components/classComponent';
 
 ReactDOM.render(
-  <Family lastName='Chung'>
-    <Member name='Camila'/>
-    <Member name='Juliana'/>
-    <Member name='Sônia'/>
-    <Member name='Júlio'/>
-  </Family>,
+  <ClassComponent value='Componente Classe' />,
   document.getElementById('root')
 );
 
